8946af49385743f66ff72831e372d4b0d23d22c7
dudwn7504 <dudwn7504@naver.com>
2020-06-09 14:18:53 +0900

    b1 upload

---
+++ b1/main.js
@@ -0,0 +1,8 @@
+    function ani() {
+        $(".ban").not(":animated").animate({"margin-left":"-1280px"},500, function(){
+            $(".ban li").eq(0).appendTo(".ban");
+            $(".ban").css("margin-left","0");
+        });
+    }
+
+    var intv = setInterval(function(){ ani(); }, 4000);

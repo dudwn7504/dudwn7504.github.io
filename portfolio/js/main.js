0423ee6c73e125ae21d8690b936e9fe09e2b0a32
dudwn7504 <47254286+dudwn7504@users.noreply.github.com>
2020-10-29 13:55:48 +0900

    Add files via upload
    portfolio upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,96 @@
+//사이드메뉴바에서 메뉴클릭시 해당화면으로 이동
+$(function() {    
+    $(".menu li:eq(0)").click(function(){
+    $("html,body").animate({ scrollTop: 1200* $(this).index() });
+    });
+    $(".menu li:eq(1)").click(function(){
+        $("html,body").animate({ scrollTop: 750* $(this).index() });
+    });
+    $(".menu li:eq(2)").click(function(){
+        $("html,body").animate({ scrollTop: 930* $(this).index() });
+    });
+    $(".menu li:eq(3)").click(function(){
+        $("html,body").animate({ scrollTop: 1300* $(this).index() });
+    });
+    // $(".menu li:last").click(function(){
+    //     $("html,body").animate({ scrollTop: 1420* $(this).index() });
+    // });
+});
+
+//사이드메뉴바에 해당화면에 해당하는 메뉴가 bold가 됨.
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var idx = $(document).scrollTop();
+        var menu1 = $("#page1").height();
+        var menu2 = menu1+$("#page2").height();
+        var menu3 = menu2+$("#page3").height();
+        var menu4 = menu3+$("#page4").height();
+
+        if(idx < menu1) {
+            $("#intro").addClass("on");
+            $("#profile").removeClass("on");
+            $("#portfolio").removeClass("on");
+            $("#contact").removeClass("on");
+        } else if(idx < menu2) {
+            $("#profile").addClass("on");
+            $("#intro").removeClass("on");
+            $("#portfolio").removeClass("on");
+            $("#contact").removeClass("on");
+        } else if(idx < menu3) {
+            $("#portfolio").addClass("on");
+            $("#intro").removeClass("on");
+            $("#profile").removeClass("on");
+            $("#contact").removeClass("on");
+        } else if(idx < menu4) {
+            $("#contact").addClass("on");
+            $("#intro").removeClass("on");
+            $("#profile").removeClass("on");
+            $("#portfolio").removeClass("on");
+
+        } 
+    });
+});
+
+//포트폴리오 해당항목 클릭시 작동하는 jquery
+$(document).ready(function() {
+    // $(".portpage_img").addClass("img_before");
+    $(".img_tit:eq(0)").click(function() {
+        $(this).animate({opacity:'0', marginLeft:'0px'},600);
+        $(".portpage_img_box:eq(0)").animate({marginLeft:'0px',},600);
+        $(".portpage_txt:eq(0)").delay(600).fadeIn(600);
+    });
+    $(".img_tit:eq(1)").click(function() {
+        $(this).animate({opacity:'0', marginLeft:'0px'},600);
+        $(".portpage_img_box:eq(1)").animate({marginLeft:'0px',},600);
+        $(".portpage_txt:eq(1)").delay(600).fadeIn(600);
+    });
+    $(".img_tit:eq(2)").click(function() {
+        $(this).animate({opacity:'0', marginLeft:'0px'},600);
+        $(".portpage_img_box:eq(2)").animate({marginLeft:'0px',},600);
+        $(".portpage_txt:eq(2)").delay(600).fadeIn(600);
+    });
+    $(".img_tit:eq(3)").click(function() {
+        $(this).animate({opacity:'0', marginLeft:'0px'},600);
+        $(".portpage_img_box:eq(3)").animate({marginLeft:'0px',},600);
+        $(".portpage_txt:eq(3)").delay(600).fadeIn(600);
+    });
+});
+
+//스킬파이 애니메이션
+$(function() {
+    $('.chart').kimsPieChart({
+      scaleColor: "#ecf0f1",
+      lineWidth: 20,
+      lineCap: 'butt',
+      barColor: '#1abc9c',
+      trackColor: "#ecf0f1",
+      size: 160,
+      animate: 500
+    });
+  });
+$(".chart").each(function(){
+    let txt1 = $(this).attr("data-percent");
+    $(this).html("<span>"+$(this).text()+"</span><span>"+txt1+"</span>");
+    $(this).find("span").css("display", "block").css("text-align", "center");
+    $(this).find("span").eq(0).css("padding-top", "33%");
+});
